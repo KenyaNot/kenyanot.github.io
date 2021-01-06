839a9b393f185424d9a7f639f7dbeabbdb2a1dbd
KenyaNot <72592190+KenyaNot@users.noreply.github.com>
2021-01-06 18:40:46 -0500

    MOTD
    Implemented an MOTD at the top of the page for flavor. Testing has confirmed the client to work with Google Drive links provided an API key is provided

--- js/index.js
+++ js/index.js
@@ -17,3 +17,20 @@ var addItem = function(name, url, type){
 player.playlist(queue);
 player.playlistUi();
 player.playlist.autoadvance(0);
+
+
+var motdList = [
+	"Don't Dead, Open Inside",
+	"Don't cry for me, I'm already dead",
+	"Serving up spooks 24/7",
+	"Double dead!",
+	"Get cranking!",
+	"What do you mean I'm just a webpage?"
+];
+
+var getRandom = function(bound){
+	return Math.floor(Math.random() * Math.floor(bound));
+}
+
+document.getElementById('motd').innerHTML = motdList[getRandom(motdList.length)];
+
