cfdb0368cdf99f04b0caf533e6f95e35ed47d8ab
KenyaNot <72592190+KenyaNot@users.noreply.github.com>
2021-04-11 16:51:32 -0400

    Minor Tweaks
    Changed font, prettified comments and code layout, removed redundant JS code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
-// Init player and take input from form
+// Init player
 var player = videojs('video');
 videojs(document.querySelector('video')).playlistUi();
 queue = [];
+
+// Add items to the playlist queue
 var addItem = function(name, url, type){
 	queue.push({
 		name: name,
@@ -10,7 +12,8 @@ var addItem = function(name, url, type){
 			type: type
 		}]
 	})
-	player.playlist(queue);}
+	player.playlist(queue);
+}
 player.playlist(queue);
 player.playlistUi();
 player.playlist.autoadvance(0);
@@ -37,19 +40,16 @@ var motdList = [
 	"I'm in love with an ant!",
 	"Don't lose your way",
 	"Makes your kokoro go doki doki",
-	"This stream brought to you by Raid: Shadow Legends"];
+	"This stream brought to you by Raid: Shadow Legends",
+	"I have named him Peace, in the hope that he will bring it",
+	"Where we're going, we don't need copyright law"];
 var getRandom = function(bound){return Math.floor(Math.random() * Math.floor(bound));}
 document.getElementById('motd').innerHTML = motdList[getRandom(motdList.length)];
 
-// Take normal GDrive links and add the proper google API link to playlist - EXPERIMENTAL
-// If successful, fold into addItem
+// Take normal GDrive links and add the proper google API link to playlist
 var getAPILink = function(link){
-	var linkFormat = "https://www.googleapis.com/drive/v3/files/FILE_ID/?key=AIzaSyAebO7daZ4oEQw3XNcO7Jim_9YPAmOb1rI&alt=media"
 	var fragment = link.match(/[-\w]{25,}/);
-	console.log("" + fragment);
 	var newLink = "https://www.googleapis.com/drive/v3/files/";
 	newLink = newLink.concat(fragment, "/?key=AIzaSyAebO7daZ4oEQw3XNcO7Jim_9YPAmOb1rI&alt=media");
 	return newLink;
 }
-
-// test function call: getAPILink("https://drive.google.com/file/d/1EEjd0wHeDfYvNxT5rEfeYiLQHfG_Z0ps/view?usp=sharing");
